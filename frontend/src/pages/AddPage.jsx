fdf119fbd4270530609883cc70e8d416ea4291a6
Fracas <73628743+Fr4cas@users.noreply.github.com>
2025-09-18 08:56:40 +0800

    feat: add navbar to all pages

--- frontend/src/pages/AddPage.jsx
+++ frontend/src/pages/AddPage.jsx
@@ -1,5 +1,12 @@
-function AddPage() {
+import Navbar from "../components/Navbar"
 
+function AddPage() {
+    return (
+        <>
+            <Navbar />
+            <h1>Add Page</h1>
+        </>
+    )
 }
 
 export default AddPage
--- frontend/src/pages/ViewPage.jsx
+++ frontend/src/pages/ViewPage.jsx
@@ -1,5 +1,12 @@
+import Navbar from "../components/Navbar"
+
 function ViewPage() {
-    
+    return (
+        <>
+            <Navbar />
+            <h1>View Page</h1>
+        </>
+    )
 }
 
 export default ViewPage
