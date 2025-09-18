03f7ad15db26b1ce4f646561d2569e084727344a
Fracas <73628743+Fr4cas@users.noreply.github.com>
2025-09-18 09:26:15 +0800

    feat: add MemberTable component

--- frontend/src/pages/ViewPage.jsx
+++ frontend/src/pages/ViewPage.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../components/Navbar"
+import MemberTable from "../components/MemberTable"
 
 function ViewPage() {
     return (
         <>
             <Navbar />
             <h1>View Page</h1>
+            <MemberTable />
         </>
     )
 }
