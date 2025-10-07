057f7387bf12fa249d321c0e8db41daba87deb51
Fracas <chenghsin2007@gmail.com>
2025-10-07 02:49:55 +0100

    chore: remove MUI dependencies

--- frontend/src/pages/ViewPage.jsx
+++ frontend/src/pages/ViewPage.jsx
@@ -1,11 +1,9 @@
 import Navbar from "../components/Navbar"
-import MemberTable from "../components/MemberTable"
 
 function ViewPage() {
     return (
         <>
             <Navbar />
-            <MemberTable />
         </>
     )
 }
