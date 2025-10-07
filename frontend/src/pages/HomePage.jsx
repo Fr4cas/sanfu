64a849a02204799bc074c05d35e1976cbf232d29
Fracas <chenghsin2007@gmail.com>
2025-10-07 03:20:57 +0100

    chore: install tailwindcss dependency

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ function HomePage() {
         <>
             <Navbar />
             <h1>Sanfu Data</h1>
+            <h2 className="bg-red-500 text-3xl font-bold text-black">Tailwind is working </h2>
         </>
     )
 }
