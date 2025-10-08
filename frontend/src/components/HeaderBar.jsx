0d327ed4828d7ded56a1ac0c4169da601e210b74
Fracas <chenghsin2007@gmail.com>
2025-10-08 06:33:04 +0100

    feat: add new design layout shell to HomePage

--- frontend/src/components/HeaderBar.jsx
+++ frontend/src/components/HeaderBar.jsx
@@ -2,7 +2,7 @@ function HeaderBar() {
     return (
         <header className="flex justify-between items-center mb-8">
             <div>
-                <h1 className="text-3xl font-bold text-gray-900">Donation Dashboard</h1>
+                <h1 className="text-3xl font-bold text-gray-900">Sanfu Data Dashboard</h1>
                 <p className="text-gray-600">Track and manage member contributions</p>
             </div>
         </header>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,9 +3,14 @@ import HeaderBar from '../components/HeaderBar.jsx'
 function HomePage() {
     return (
         <>
-            <HeaderBar />
-            <h1>Sanfu Data</h1>
-            <h2 className="bg-red-500 text-3xl font-bold text-black">Tailwind is working </h2>
+            <div className="bg-gray-50 min-h-screen">
+                <div className="max-w-7x1 mx-auto px-4 sm:px-6 lg:px-8 py-8">
+                    <HeaderBar />
+                    <div className="bg-white rounded-lg shadow p-6 text-gray-600">
+                            Place to put the data
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
